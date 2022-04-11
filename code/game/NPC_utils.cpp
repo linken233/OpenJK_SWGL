8a25c7eeec295f592370963b7ad06ff2979a4887
Linken233 <linken233@hotmail.com>
2022-04-11 09:28:47 -0400

    Player jetpack fix
    If a character is meant to have a jetpack but doesn't have one, they can fly. NPCs can't though.

--- code/game/NPC_utils.cpp
+++ code/game/NPC_utils.cpp
@@ -1694,7 +1694,7 @@ void JET_FlyStart( gentity_t *self )
 	self->lastInAirTime = level.time;
 	if (self->client->NPC_class == CLASS_BOBAFETT || self->client->NPC_class == CLASS_MANDALORIAN || self->client->NPC_class == CLASS_JANGO)
 	{
-		if (self->genericBolt1 != -1 && self->genericBolt2 != -1)
+		if (self == player || (self->genericBolt1 != -1 && self->genericBolt2 != -1))
 			Boba_FlyStart( self );
 	}
 	else if ( self->client->NPC_class == CLASS_ROCKETTROOPER )
