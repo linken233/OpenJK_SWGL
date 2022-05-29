08c98564cfdfa747ffc7b65544bc31bb4187a58e
Linken233 <linken233@hotmail.com>
2022-05-29 04:18:41 -0400

    UI improvements
    New UIScripts "savepage" "gotopage" "rgbsabercvars" "recordcharweapons" and "applycharweapons"
    
    SavePage: Saves the last page the user was on for a specific menu.
    
    GoToPage: Automatically directs the player to the last page they were on for a specific menu. If there's no page, default to "Page1"
    
    RGBSaberCvars: Workaround so characters with rgb sabers will display their saber properly in the menu.
    
    RecordCharWeapons: Records the character's weapons (set in the menu), maximum of 4 weapons.
    
    ApplyCharWeapons: Applies the weapons as recorded in the previous script.

--- code/ui/ui_main.cpp
+++ code/ui/ui_main.cpp
@@ -57,6 +57,7 @@ extern qboolean UI_SaberModelForSaber( const char *saberName, char *saberModel )
 extern qboolean UI_SaberSkinForSaber( const char *saberName, char *saberSkin );
 extern void UI_SaberAttachToChar( itemDef_t *item );
 extern void Menu_SetItemText(const menuDef_t* menu, const char* itemName, const char* text);
+extern void Menu_ShowItemByName(menuDef_t* menu, const char* p, qboolean bShow);
 
 extern qboolean PC_Script_Parse(const char **out);
 
@@ -112,6 +113,7 @@ static void		UI_UpdateCharacterCvars ( void );
 static void		UI_GetCharacterCvars ( void );
 static void		UI_GetCharacterCustomization( void );
 static void		UI_GetNPCCvars(void);
+static void		UI_RGBSaberCvars(void);
 static void		UI_UpdateSaberCvars ( void );
 static void		UI_GetSaberCvars ( void );
 static void		UI_ResetSaberCvars ( void );
@@ -174,6 +176,10 @@ void			UI_ResetDefaults( void );
 void			UI_AdjustSaveGameListBox( int currentLine );
 
 void			Menus_CloseByName(const char *p);
+
+static void UI_ApplyCharWeapons(void);
+
+static void UI_RecordCharWeapons(const char** args);
 
 // Movedata Sounds
 enum
@@ -361,6 +367,11 @@ qboolean openedAngles;
 const char* npcCode;
 
 const char* missionCode;
+
+int weaponOne;
+int weaponTwo;
+int weaponThree;
+int weaponFour;
 
 static void UI_RegisterCvars( void );
 void UI_Load(void);
@@ -1341,6 +1352,26 @@ static qboolean UI_RunMenuScript ( const char **args )
 		{
 			ui.Cmd_ExecuteText(EXEC_APPEND, va("devmap %s\n", missionCode));
 		}
+		else if (Q_stricmp(name, "savePage") == 0)
+		{
+			const char* page;
+			String_Parse(args, &page);
+			menuDef_t* savedMenu = Menu_GetFocused();
+			savedMenu->savedPage = page;
+		}
+		else if (Q_stricmp(name, "goToSavedPage") == 0)
+		{
+			menuDef_t* menu = Menu_GetFocused();
+
+			if (menu->savedPage)
+			{
+				Menu_ShowItemByName(menu, menu->savedPage, qtrue);
+			}
+			else
+			{
+				Menu_ShowItemByName(menu, "Page1", qtrue);
+			}
+		}
 		else if (Q_stricmp(name, "getcharcustom") == 0)
 		{
 			UI_GetCharacterCustomization();
@@ -1352,6 +1383,10 @@ static qboolean UI_RunMenuScript ( const char **args )
 		else if (Q_stricmp(name, "updatesabercvars") == 0)
 		{
 			UI_UpdateSaberCvars();
+		}
+		else if (Q_stricmp(name, "rgbsabercvars") == 0)
+		{
+			UI_RGBSaberCvars();
 		}
 		else if (Q_stricmp(name, "getsabercvars") == 0)
 		{
@@ -1453,6 +1488,14 @@ static qboolean UI_RunMenuScript ( const char **args )
 		else if (Q_stricmp(name, "clearweapons") == 0)
 		{
 			UI_ClearWeapons();
+		}
+		else if (Q_stricmp(name, "recordcharweapons") == 0)
+		{
+			UI_RecordCharWeapons(args);
+		}
+		else if (Q_stricmp(name, "applycharweapons") == 0)
+		{
+			UI_ApplyCharWeapons();
 		}
 		else if (Q_stricmp(name, "stopgamesounds") == 0)
 		{
@@ -4631,6 +4674,29 @@ static void UI_UpdateSaberCvars ( void )
 
 }
 
+static void UI_RGBSaberCvars(void)
+{
+	saber_colors_t saberColour = TranslateSaberColor(Cvar_VariableString("ui_saber_color"));
+
+	if (saberColour >= SABER_RGB)
+	{
+
+		Cvar_SetValue("ui_rgb_saber_red", (saberColour & 0xff));
+		Cvar_SetValue("ui_rgb_saber_green", ((saberColour >> 8) & 0xff));
+		Cvar_SetValue("ui_rgb_saber_blue", ((saberColour >> 16) & 0xff));
+	}
+
+	saber_colors_t saber2Colour = TranslateSaberColor(Cvar_VariableString("ui_saber2_color"));
+
+	if (saber2Colour >= SABER_RGB)
+	{
+
+		Cvar_SetValue("ui_rgb_saber2_red", (saber2Colour & 0xff));
+		Cvar_SetValue("ui_rgb_saber2_green", ((saber2Colour >> 8) & 0xff));
+		Cvar_SetValue("ui_rgb_saber2_blue", ((saber2Colour >> 16) & 0xff));
+	}
+}
+
 #ifndef JK2_MODE
 static void UI_UpdateFightingStyleChoices ( void )
 {
@@ -5705,6 +5771,72 @@ static void UI_ResetCharacterListBoxes( void )
 			item->cursorPos = 0;
 		}
 	}
+}
+
+static void UI_RecordCharWeapons(const char **args)
+{
+	// Let's clear the weapons first
+	weaponOne = 0;
+	weaponTwo = 0;
+	weaponThree = 0;
+	weaponFour = 0;
+
+
+	// Record 4 weapons (lightsaber is given in the menu if the character has one already, and every character gets melee regardless)
+	// This is a really ugly way of doing this...
+	const char* firstWeapon;
+	String_Parse(args, &firstWeapon);
+	weaponOne = atoi(firstWeapon);
+
+	const char*secondWeapon;
+	String_Parse(args, &secondWeapon);
+	weaponTwo = atoi(secondWeapon);
+
+	const char* thirdWeapon;
+	String_Parse(args, &thirdWeapon);
+	weaponThree = atoi(thirdWeapon);
+
+	const char* fourthWeapon;
+	String_Parse(args, &fourthWeapon);
+	weaponFour = atoi(fourthWeapon);
+
+}
+
+static void UI_ApplyCharWeapons()
+{
+	// Get player state
+	client_t* cl = &svs.clients[0];	// 0 because only ever us as a player
+
+	if (!cl)	// No client, get out
+	{
+		return;
+	}
+
+	if (cl->gentity && cl->gentity->client)
+	{
+		playerState_t* pState = cl->gentity->client;
+
+		// Every character gets melee, no matter what
+		pState->weapons[WP_MELEE] = 1;
+
+		// Again, super ugly, wish I could do it less horribly
+		if (weaponOne)
+			pState->weapons[weaponOne] = 1;
+		if (weaponTwo)
+			pState->weapons[weaponTwo] = 1;
+		if (weaponThree)
+			pState->weapons[weaponThree] = 1;
+		if (weaponFour)
+			pState->weapons[weaponFour] = 1;
+		
+	}
+
+	weaponOne = 0;
+	weaponTwo = 0;
+	weaponThree = 0;
+	weaponFour = 0;
+	//for(int i = 2; i < )
+	//weapons & (1 << FP_PUSH)
 }
 
 static void UI_ClearInventory ( void )
